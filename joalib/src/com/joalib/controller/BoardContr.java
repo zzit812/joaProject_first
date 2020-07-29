a0ad8911e11102052f0d0a009f8447f7befacce8
Administrator <Administrator@321-11>
2020-07-29 14:55:23 +0900

    MVC2 구조를 만들다가.. 혹시 터질까봐 두려워서 일단 멈추고 개인파일로 해보겠다

---
+++ joalib/src/com/joalib/controller/BoardContr.java
@@ -0,0 +1,24 @@
+package com.joalib.controller;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.joalib.DTO.ActionForward;
+
+public class BoardContr extends javax.servlet.http.HttpServlet
+{
+	protected void doProsecc(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		String RequestURI=request.getRequestURI();
+		String contextPath=request.getContextPath();
+		String command=RequestURI.substring(contextPath.length());
+		ActionForward forward=null;
+		
+	}
+}
