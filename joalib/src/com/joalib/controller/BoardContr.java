f6934002c56eb85ab0758dbb0b7cdb7c01f21d34
Administrator <Administrator@321-11>
2020-07-30 14:23:55 +0900

    게시판관련 수정중

--- joalib/src/com/joalib/controller/BoardContr.java
+++ joalib/src/com/joalib/controller/BoardContr.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.joalib.controller;
 
 import java.io.IOException;
@@ -72,28 +71,3 @@ public class BoardContr extends javax.servlet.http.HttpServlet
 		doProcess(request,response);
 	} 
 }
-=======
-package com.joalib.controller;
-
-import java.io.IOException;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.joalib.DTO.ActionForward;
-
-public class BoardContr extends javax.servlet.http.HttpServlet{
-	protected void doProsecc(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		
-		request.setCharacterEncoding("UTF-8");
-		String RequestURI=request.getRequestURI();
-		String contextPath=request.getContextPath();
-		String command=RequestURI.substring(contextPath.length());
-		ActionForward forward=null;
-		
-	}
-}
->>>>>>> refs/remotes/origin/master
